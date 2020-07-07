633fb9a376dfe34c5faad6296e70803dfee96a5f
Dang Nguyen <danghwin@gmail.com>
2020-07-07 23:22:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,10 +38,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+#   def self.create(name:, grade:)
+#     student = Student.new(name, grade)
+#     student.save
+#     student
+#   end
   
-end
+# end
